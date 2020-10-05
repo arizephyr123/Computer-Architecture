415c904517f013b18764b5525699b325ea6033b3
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-10-04 19:23:51 -0700

    CPU class init, run completed

---
+++ ls8/Inventory.md
@@ -0,0 +1,14 @@
+cpu.py
+main cpu class methods:
+
+- load -> loads program to memory
+- alu ->
+
+todo:
+
+- init: ram, reg, pc, fl, ie??
+- ram_read()
+- run
+- alu operations: SUB, MULT, DIV, MOD
+
+==============
--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,33 +2,55 @@
 
 import sys
 
+
+
+
+HLT = 0b00000001  # halt CPU, exit emulator
+LDI = 0b10000010  # load "immediate" - set this register to this value
+PRN = 0b01000111  # prints numeric value stored in register
+# ALU methods
+ADD = 0b10100000
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0 # program counter
+        self.running = False
+        self.ir_methods = {
+            HLT: self.hlt,
+            LDI: self.ldi,
+            PRN: self.prn,
+            ADD: self.add,
+            MUL: self.mul,
+
+        }
+        # fl = 
+        # ie = 
+
         pass
 
-    def load(self):
+    def load(self, file_path):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        with open(file_path) as program:
+            for line in program:
+                instruction = line.strip().split()
+                if len(instruction) == 0 or instruction[0] == '#':
+                    continue
+                try:
+                    # 
+                    self.ram[address] = int(instruction[0], 2)
+                except ValueError:
+                    print('Invalid command: {instruction[0]}')
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        address +=1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -60,6 +82,45 @@ class CPU:
 
         print()
 
+
+    def ram_read(self, MAR):
+        """Read RAM from memory address register(MAR)."""
+        print(self.reg[MAR])
+
+    def ram_write(self, MAR, MDR):
+        """Write RAM from memory data register to MAR"""
+        self.reg[MAR] = MDR
+
+    def hlt(self):
+        """ halt CPU, exit emulator """
+        self.running = False
+        sys.exit()
+
+    def ldi(self):
+        """ load "immediate" - set this register to this value """
+        pass
+
+    def prn(self):
+        """  """
+        pass
+
+    def add(self):
+        """  """
+        pass
+
+    def mul(self):
+        """  """
+        pass
+
+
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+
+        while self.running == True:
+            # ir => instruction register
+            # in ram at program counter index
+            ir = self.ram[self.pc]
+
+            if ir in self.ir_methods:
+                self.ir_methods()
